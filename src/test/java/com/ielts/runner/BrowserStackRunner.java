75da553d62b97396e0f2082a2718abf061c3621d
neralwar <50049068+neralwar@users.noreply.github.com>
2020-04-02 12:41:27 +0530

    Update BrowserStackRunner.java
    Updated runner file to point to UI feature file only

--- src/test/java/com/ielts/runner/BrowserStackRunner.java
+++ src/test/java/com/ielts/runner/BrowserStackRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-		 features = {"src/test/resources/features/UI_Automation/Booking/organisation_Search.feature","src/test/resources/features/RestService_Automation/Employee/employee_management.feature"}
-		 ,glue={"com/ielts/ui/stepdefinition","com.ielts.restservice.stepdefinition"}
+		 features = {"src/test/resources/features/UI_Automation/Booking/organisation_Search.feature"}
+		 ,glue={"com/ielts/ui/stepdefinition"}
 		 
 		 )
 public class BrowserStackRunner extends BrowserStackSerenityTest {
